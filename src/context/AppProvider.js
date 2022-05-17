f8bfc7533b1957bf8f1e335c252085ad2dc79e24
bh10-d <duchieubui25012002@gmail.com>
2022-05-17 18:34:15 +0700

    change path n

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,4 +1,4 @@
-import React,{ useEffect, useState } from 'react';
+import React,{ useState } from 'react';
 import {AuthContext} from '../Context/AuthProvider';
 import useFirestore from '../Hooks/useFirestore';
 
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,6 +1,6 @@
 import React,{ useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { auth } from '../Firebase/config';
+import { auth } from '../../src/Firebase/config';
 import { Spin } from 'antd';
 
 
